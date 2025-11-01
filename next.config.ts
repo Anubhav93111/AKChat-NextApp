5296570acf863f7c2429ce8aab3ebb9dc5be6cea
Anubhav Kumar <anubhavkumar93111@gmail.com>
2025-11-01 20:45:01 +0530

    @ alias deployment error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+  // You can add other Next.js config options here if needed
 };
 
 export default nextConfig;
